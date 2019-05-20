7cc028936d6a31d81ec59bbd1d3afd59a93b5293
Jonathan Borzilleri <jonathan@borzilleri.net>
2019-05-19 17:39:52 -0700

    rework filebot/amc options handling

--- file-renamer/rename.py
+++ file-renamer/rename.py
@@ -12,32 +12,37 @@ CONFIG = json.load(open(CONFIG_FILE))
 parser = argparse.ArgumentParser()
 parser.add_argument("scan_dir")
 parser.add_argument("-a", "--action", choices=["move", "copy", "test"], default="move")
-parser.add_argument("-c", "--conflict", choices=["auto", "skip", "override"], default="auto")
+parser.add_argument(
+    "-c", "--conflict", choices=["auto", "skip", "override"], default="auto"
+)
 args = parser.parse_args()
 scan_path = Path(args.scan_dir).resolve()
 
 if not os.path.isdir(scan_path):
     sys.exit("Scan path is not a directory: {}".format(scan_path))
 
-if not os.path.isdir(CONFIG['output']['dir']):    
-    sys.exit("Output dir is not a directory: {}".format(CONFIG['output']['dir']))
+if not os.path.isdir(CONFIG["output"]["dir"]):
+    sys.exit("Output dir is not a directory: {}".format(CONFIG["output"]["dir"]))
 
-if CONFIG['output']['isMount'] and not os.path.ismount(CONFIG['output']['dir']):
-    sys.exit("Output dir is not mounted: {}".format(CONFIG['output']['dir']))
+if CONFIG["output"]["isMount"] and not os.path.ismount(CONFIG["output"]["dir"]):
+    sys.exit("Output dir is not mounted: {}".format(CONFIG["output"]["dir"]))
 
-print("Scanning dir: '{}'; Action: '{}'; Conflict strategy: '{}'".format(scan_path, args.action, args.conflict), flush=True)
+print(
+    "Scanning dir: '{}'; Action: '{}'; Conflict strategy: '{}'".format(
+        scan_path, args.action, args.conflict
+    ),
+    flush=True,
+)
 
-AMC_OPTIONS = {"--def {}={}".format(k,v) for (k,v) in CONFIG['amc']['options'].items()}
-
-CMD_ARGS = [
-    "filebot",
+FILEBOT_OPTIONS = [
     "-script fn:amc",
     "--action {}".format(args.action),
     "-non-strict",
     "--conflict {}".format(args.conflict),
-    "--output {}".format(CONFIG['output']['dir']),
-    "--def plex={}:{}".format(CONFIG['plex']['host'], CONFIG['plex']['token']),
-    " ".join(AMC_OPTIONS),
-    str(scan_path)
+    "--output {}".format(CONFIG["output"]["dir"]),
+    "--def @{}".format(CONFIG["amc"]["options"]),
 ]
-subprocess.run(CMD_ARGS)
+
+CMD = "filebot {opts} {dir}".format(opts=" ".join(FILEBOT_OPTIONS), dir=str(scan_path))
+print("Running shell command: '{}'".format(CMD))
+subprocess.run(CMD)
